7807d25f9f6bf41d759f9628e941ddaca5bec9f5
RedstoneFreak <email.aw@web.de>
2025-04-07 19:02:33 +0200

    Zahlen eingefügt

--- src/Sudoku/GUI.java
+++ src/Sudoku/GUI.java
@@ -2,6 +2,8 @@ package Sudoku;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -11,11 +13,16 @@ public class GUI extends JFrame {
     boolean Menu = true;
     int padding = 50;
     int FX1, FY1, FX2, FY2, Fh, Fw;
+    int fontSize = 1;
     public int[][] feld;
+    JLabel[][] labels;
+    boolean drawn = false;
+    int activerow, activecol;
     public GUI(int width, int height, int[][] feld){
         this.width = width;
         this.height = height;
         this.feld = feld;
+        labels = new JLabel[9][9];
         create();
     }
 
@@ -23,7 +30,7 @@ public class GUI extends JFrame {
         //the Window
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);     //if the window gets closed the projects closes
         //setImageIcon
-        setResizable(true);                             //makes the window resizable
+        setResizable(true);                              //makes the window resizable
         setVisible(true);                               //makes the window visible
         setTitle("Sudoku");                          //sets the name
 
@@ -31,11 +38,21 @@ public class GUI extends JFrame {
         setLocationRelativeTo(null);                    //loc
         setLayout(null);                                //needs for the JPanel
 
+
         pan = new PrivJPanel();                  //creates the JPanel
         pan.setBounds(0,0,getWidth(),getHeight());//sets the Bounds of the JPanel
         add(pan);                                       //adds the pan to the JFrame as a Component
 
-        addMouseListener(new PrivMouseListener());      //sets the MouseListener
+        for (int i = 0; i < 9; i++) {
+            for (int u = 0; u < 9; u++) {
+                labels[i][u] = new JLabel();
+            }
+        }
+        drawn = true;
+        NewSudoku();
+
+        addMouseListener(new InputHandler());      //sets the MouseListener
+        addKeyListener(new InputHandler());
     }
 
     private void reloadVars(){
@@ -45,35 +62,24 @@ public class GUI extends JFrame {
         FX2 = getWidth()*2/3-padding;
         if (FX2 >= (getHeight()-padding)){
             FY2 = getHeight()-padding;
+            FX2 = FY2;
         }else{
             FY2 = FX2;
         }
         Fh = FY2-FY1;
         Fw = FX2-FX1;
+
+        fontSize = Math.min(Fw, Fh) / 11;
     }
 
     private class PrivJPanel extends JPanel{
         Graphics2D g;
-        //private JLabel[][] labels;
 
         public void paintComponent(Graphics graphics){
             super.paintComponent(graphics);
             g = (Graphics2D) graphics;
             reloadVars();
             //Draw
-
-            /*int newSize = Math.min(getWidth(), getHeight()) / 10;
-            int oldSize = 0;
-            if (newSize != oldSize) {
-                for (int i = 0; i < labels.length; i++) {
-                    for (int u = 0; u < labels[i].length; u++){
-                        labels[i][u].setFont(new Font("Arial", Font.PLAIN, newSize));
-                    }
-                }
-                oldSize = newSize;
-            }*/
-
-
 
             drawQuadrat(FX1, FY1, FX2, FY2, 3);
             drawrasta(FX1, FY1, FX2, FY2, 3);
@@ -91,6 +97,18 @@ public class GUI extends JFrame {
             drawrasta(Fw/3+FX1, Fh*2/3+FY1, Fw*2/3+FX1, Fh+FY1, 1);
             drawrasta(Fw*2/3+FX1, Fh*2/3+FY1, Fw+FX1, Fh+FY1, 1);
 
+            if (drawn) {
+                for (int i = 0; i < labels.length; i++) {
+                    for (int u = 0; u < labels[i].length; u++) {
+                        labels[i][u].setFont(new Font("Arial", Font.BOLD, fontSize));
+                        labels[i][u].setLocation(Fw * (u) / 9 + FX1, Fh * (i) / 9 + FY1);
+                        labels[i][u].setSize((FX2 - FX1) / 9, (FY2 - FY1) / 9);
+                        labels[i][u].setVisible(true);
+                        labels[i][u].setOpaque(false);
+                        add(labels[i][u]);
+                    }
+                }
+            }
 
 
 
@@ -112,7 +130,24 @@ public class GUI extends JFrame {
         }
     }
 
-    private class PrivMouseListener implements MouseListener{
+    private void killFrame(){
+        dispose();
+    }
+
+    public void NewSudoku(){
+        for (int i = 0; i < feld.length; i++) {
+            for (int u = 0; u < feld[i].length; u++) {
+                if (feld[i][u] != 0) {
+                    labels[i][u].setVerticalAlignment(SwingConstants.CENTER);
+                    labels[i][u].setHorizontalAlignment(SwingConstants.CENTER);
+                    labels[i][u].setText("" + feld[i][u] + "");
+                }
+            }
+        }
+    }
+
+    private class InputHandler implements MouseListener, KeyListener{
+        boolean Active;
 
         @Override
         public void mouseClicked(MouseEvent e) {
@@ -137,9 +172,23 @@ public class GUI extends JFrame {
         @Override
         public void mouseExited(MouseEvent e) {
         }
+
+
+        //Keys
+        @Override
+        public void keyTyped(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+
+        }
     }
 
-    private void killFrame(){
-        dispose();
-    }
 }
